e8e7ae44f8f34f6a468a7a5971a15930be2490e5
a <6@qq.com>
2024-07-06 10:35:27 +0800

    initB

--- src/main/java/com/mikael/web/component/ServiceComponent.java
+++ src/main/java/com/mikael/web/component/ServiceComponent.java
@@ -12,9 +12,9 @@ public class ServiceComponent implements ApplicationListener<ServletRequestHandl
     public void onApplicationEvent(ServletRequestHandledEvent event) {
         Throwable failureCause = event.getFailureCause();
         if (failureCause != null) {
-            System.out.printf( "请求时发生的错误原因：$S",failureCause.getMessage());
+            System.out.printf( "请求时发生的错误原因：%S",failureCause.getMessage());
         }
-        System.err.printf(" 请求客户端地址: $S,请求URL: $S,请求Method：$S,请求耗时：$d%n",event.getClientAddress(),event.getRequestUrl(),event.getMethod(),event.getProcessingTimeMillis());
+        System.err.printf(" 请求客户端地址: %S,请求URL: %S,请求Method：%S,请求耗时：%d%n",event.getClientAddress(),event.getRequestUrl(),event.getMethod(),event.getProcessingTimeMillis());
     }
 
     @Override
