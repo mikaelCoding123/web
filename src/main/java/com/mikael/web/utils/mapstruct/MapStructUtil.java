dad77d384de143eac4bc7365964129b62c4c3c2d
616164124@qq.com <616164124@qq.com>
2024-07-21 22:34:07 +0800

    2024年7月21日22

---
+++ src/main/java/com/mikael/web/utils/mapstruct/MapStructUtil.java
@@ -0,0 +1,14 @@
+package com.mikael.web.utils.mapstruct;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MapStructUtil {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int a = ThreadLocalRandom.current().nextInt(100);
+            System.out.println(a);
+
+        }
+    }
+}
