d09d29471ee5775f76fd680b92d42224360f91ae
Administrator <2@qq.com>
2024-12-01 10:50:22 +0800

    Revert "RestControllerAdvice容易被切面吞了异常导致不执行"
    This reverts commit 3b7c67998b0660d5e785a1e4443b72bace6a1308.

--- src/main/java/com/mikael/web/component/conadvice/ControllerAdvice01.java
+++ src/main/java/com/mikael/web/component/conadvice/ControllerAdvice01.java
@@ -3,35 +3,26 @@ package com.mikael.web.component.conadvice;
 import com.mikael.web.utils.exception.BizException;
 import com.mikael.web.utils.result.Result;
 import com.mikael.web.utils.result.ResultUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 注意不要重复获取异常，容易形成嵌套死循环
- * <p>
- * 容易被切面吞了异常导致不执行
  */
-@Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class ControllerAdvice01 {
-    //获取到UNAUTHORIZED跳到指定的错误页面
-    @ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "找不到服务")
-    @ExceptionHandler(Exception.class)
-    public Result handleException(Exception e) {
-        log.error("ControllerAdvice01==============ArithmeticException===");
-        return ResultUtil.error(e.getMessage());
+    @ResponseStatus(code = HttpStatus.BAD_GATEWAY, reason = "找不到服务")
+    @ExceptionHandler(BizException.class)
+    public String handleException(BizException e) {
+        return "error.html";
     }
 
-    @ExceptionHandler(BizException.class)
-    public Result handleBizException(BizException e) {
-        return ResultUtil.error(e);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public Result handleException(Exception e) {
+//        return ResultUtil.error(e);
+//    }
 
 
 }
--- src/main/java/com/mikael/web/filterAndInterceptor/LoginFilter.java
+++ src/main/java/com/mikael/web/filterAndInterceptor/LoginFilter.java
@@ -1,26 +1,16 @@
 package com.mikael.web.filterAndInterceptor;
 
-import cn.hutool.http.server.HttpServerRequest;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.http.HttpRequest;
 
 
-/**
- * https://blog.csdn.net/z69183787/article/details/127808802
- * WebFilter失效
- */
 
-@Order(1)
-@WebFilter(urlPatterns = "/test01/*",filterName = "LoginFilter01")
+@WebFilter(urlPatterns = "/*", filterName = "loginFilter")
 @Slf4j
-public class LoginFilter01 implements Filter {
+public class LoginFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -28,8 +18,7 @@ public class LoginFilter01 implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest servletRequest1 = (HttpServletRequest)servletRequest;
-        log.info("LoginFilter01=====urlPatterns==========/test01/*===="+servletRequest1.getRequestURI());
+        log.info("LoginFilter===================");
         //将信息往下传
         filterChain.doFilter(servletRequest, servletResponse);
 
